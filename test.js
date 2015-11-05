1355d0607b78b25e551e3a9894c1037d53971a79
Rico Moorer <rico@alumni.upenn.edu>
2015-11-05 00:22:32 -0800

    Got the piggybacks working :D

--- test.js
+++ test.js
@@ -4,33 +4,68 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var bodyParser = require('body-parser');
-
 var instagramAccessToken = '10324727.1677ed0.e055b10af42141bb8acbb0989c259d3d';
-
+var foursquareAccessToken = 'YMLOF3SDTWE1CIRKNIMLNXJM5CWVDAERPXMV22ZIP4NYGFLQ';
 var locationId;
-
 app.use(bodyParser());
 
-var data;
+function callFourSquare(location) {
+  var queryUrl = 'https://api.foursquare.com/v2/venues/explore?near=' + location +  '&radius=3000&section=food&openNow=1&oauth_token=' + foursquareAccessToken + '&v=20151104';
 
-var url1 = 'https://api.instagram.com/v1/locations/search?foursquare_v2_id=40a55d80f964a52020f31ee3&access_token=' + instagramAccessToken
+  request(queryUrl, function(error, response, body){
+    if (error) console.log(error);
 
-request(url1, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    locationId = JSON.parse(body).data[0].id;
-    var url2 = 'https://api.instagram.com/v1/locations/' + locationId +'/media/recent?access_token=' + instagramAccessToken;
-    request2(url2);
-  }
-});
+    var items = JSON.parse(body).response.groups[0].items;
 
-function request2 (url) {
-  request(url, function(error,response,body) {
-    if (!error && response.statusCode === 200) {
-      data = body;
-      console.log(JSON.parse(body).data[0].images.standard_resolution.url);
-    }
+    var output = items.map(function(item){
+        return {
+          id: item.venue.id,
+          name: item.venue.name,
+          location: item.venue.location,
+          contact: item.venue.contact
+        }
+    });
+
+    var results = output.map(callInstagram);
+
   });
 }
+
+function callInstagram(obj){
+  var url1 = 'https://api.instagram.com/v1/locations/search?foursquare_v2_id=' + obj.id + '&access_token=' + instagramAccessToken
+
+  request(url1, function (error, response, body) {
+    if (error) console.log(error);
+
+    locationId = JSON.parse(body).data[0].id;
+
+    var url2 = 'https://api.instagram.com/v1/locations/' + locationId +'/media/recent?access_token=' + instagramAccessToken;
+
+    request(url2, function(error,response,body) {
+      if (error) console.log(error);
+
+      obj.pictures = JSON.parse(body).data.map(function(item){
+        return item.images.standard_resolution.url;
+      }).slice(0,3);
+
+      outputStuff(obj);
+
+    });
+
+  });
+
+
+}
+
+function outputStuff(arg){
+  console.log(arg);
+}
+
+callFourSquare('34.0211764,-118.4065364');
+
+
+
+
 
 // {photo1:
 // photo2:
